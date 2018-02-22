45ed4c52e17c256e14f1685957f4581a449bf784
Rafal Biegacz <rafalbiegacz@google.com>
2018-02-22 17:10:35 +0100

    replaceing tabs with spaces

--- templates/docker-entrypoint.sh
+++ templates/docker-entrypoint.sh
@@ -16,21 +16,21 @@
 
 # Add elasticsearch as command if needed
 if [ "${1:0:1}" = '-' ]; then
-	set -- elasticsearch "$@"
+        set -- elasticsearch "$@"
 fi
 
 # Drop root privileges if we are running elasticsearch
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-	# Change the ownership of user-mutable directories to elasticsearch
-	for path in \
-		/usr/share/elasticsearch/data \
-		/usr/share/elasticsearch/logs \
-	; do
-		chown -R elasticsearch:elasticsearch "$path"
-	done
-	set -- gosu elasticsearch "$@"
-	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+        # Change the ownership of user-mutable directories to elasticsearch
+        for path in \
+                /usr/share/elasticsearch/data \
+                /usr/share/elasticsearch/logs \
+        ; do
+                chown -R elasticsearch:elasticsearch "$path"
+        done
+        set -- gosu elasticsearch "$@"
+        #exec gosu elasticsearch "$BASH_SOURCE" "$@"
 fi
 
 # As argument is not related to elasticsearch,
